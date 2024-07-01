074b70114e0636c22f9de18ea0f9afea210358bf
NO WONJAE <76416118+skybaer0804@users.noreply.github.com>
2024-07-01 15:27:08 +0900

    Add : AsyncStorage와 추상화 사용

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import type { Node } from 'react';
 import {
     SafeAreaView,
@@ -10,6 +10,8 @@ import DateHead from './components/DateHead';
 import AddTodo from './components/AddTodo';
 import Empty from './components/Empty';
 import TodoList from './components/TodoList';
+import todosStorages from './storages/todosStorages';
+
 const App: () => Node = () => {
     const date = new Date();
 
@@ -19,6 +21,43 @@ const App: () => Node = () => {
         { id: 3, text: '작업환경 설정2', done: true },
     ]);
 
+    //불러오기
+    useEffect(() => {
+        todosStorages.get().then(setTodos).catch(console.error);
+    }, []);
+
+    //저장하기
+    useEffect(() => {
+        todosStorages.set(todos).catch(console.error);
+    }, [todos]);
+
+    const onInsert = (text) => {
+        const nextId =
+            todos.length > 0
+                ? Math.max(...todos.map((todo) => todo.id)) + 1
+                : 1;
+        const todo = {
+            id: nextId,
+            text, // 단축된 속성명(Shorthand property name) 문법, 현재 text 값을 파라미터로 받아오고 있는데, 이를 그대로 객체 text 속성으로 사용하겠다는 의미.
+            done: false,
+        };
+
+        setTodos(todos.concat(todo));
+    };
+
+    const onToggle = (id) => {
+        console.info('id ? ', id);
+        const nextTodos = todos.map((todo) =>
+            todo.id === id ? { ...todo, done: !todo.done } : todo
+        );
+        setTodos(nextTodos);
+    };
+
+    const onRemove = (id) => {
+        const nextTodos = todos.filter((todo) => todo.id !== id);
+        setTodos(nextTodos);
+    };
+
     return (
         <SafeAreaView style={styles.full}>
             <KeyboardAvoidingView
@@ -27,8 +66,16 @@ const App: () => Node = () => {
                 style={styles.avoid}
             >
                 <DateHead date={date} />
-                {todos.length === 0 ? <Empty /> : <TodoList todos={todos} />}
-                <AddTodo />
+                {todos.length === 0 ? (
+                    <Empty />
+                ) : (
+                    <TodoList
+                        todos={todos}
+                        onToggle={onToggle}
+                        onRemove={onRemove}
+                    />
+                )}
+                <AddTodo onInsert={onInsert} />
             </KeyboardAvoidingView>
         </SafeAreaView>
     );
